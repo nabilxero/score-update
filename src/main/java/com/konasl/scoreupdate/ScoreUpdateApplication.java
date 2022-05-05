378a7af6c0b93591faf0affa799b4e369e6fb156
Nabil Nur A Hasnat <hasnat.nabil@bjitgroup.com>
2022-05-05 13:10:09 +0600

    Initial Commit

---
+++ src/main/java/com/konasl/scoreupdate/ScoreUpdateApplication.java
@@ -0,0 +1,13 @@
+package com.konasl.scoreupdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreUpdateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoreUpdateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konasl/scoreupdate/ScoreUpdateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konasl.scoreupdate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreUpdateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
